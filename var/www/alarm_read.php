eaa58c99ab4781186acd4dc0401051e136c028e5
Jan <jackflash5500@gmail.com>
2015-01-11 19:02:49 +0100

    Added weather voice line

--- var/www/alarm_read.php
+++ var/www/alarm_read.php
@@ -7,12 +7,29 @@ fclose($handle);
 $alarms = json_decode("{\"items\": [".$contents."]}", true);
 $alarms = $alarms["items"];
 
+//Api key hack
+include("secret.php");
+
+//Weather code
+$request_url = 'https://api.forecast.io/forecast/'
+    . $apiKey
+    . '/'
+    . '53.7560'
+    . ','
+    . '9.6602'
+    .'?units=si';
+
+$response = json_decode(file_get_contents($request_url));
+$response->headers = $http_response_header;
+
 //Always needed
 exec("sudo amixer cset numid=3 1");
+
 
 foreach ($alarms as $alarm) {
     if ($alarm["time"] == (localtime()["2"].":".localtime()["1"])){
         shell_exec("sudo /var/alarm/speech.sh \"Good Morning, Jahn.\"");
+        shell_exec("sudo /var/alarm/speech.sh \"The weather outside is " . $response->currently->summary . " with " . $response->currently->temperature . " degreees out there.\"");
         shell_exec("sudo /var/alarm/speech.sh \"Playing some music!\"");
         //Security risk
         $play = 'sudo mpg123 /var/www/' . $alarm["music"];
