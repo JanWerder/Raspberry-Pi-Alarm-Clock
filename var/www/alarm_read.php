673f50791f25ab425fd8d01182771e3743ba7823
Jan <jackflash5500@gmail.com>
2015-01-11 17:49:42 +0100

    Fixed the TTS command

--- var/www/alarm_read.php
+++ var/www/alarm_read.php
@@ -12,8 +12,8 @@ exec("sudo amixer cset numid=3 1");
 
 foreach ($alarms as $alarm) {
     if ($alarm["time"] == (localtime()["2"].":".localtime()["1"])){
-        shell_exec("/var/alarm/speed.sh \"Good Morning, Jahn.\"");
-        shell_exec("/var/alarm/speed.sh \"Playing some music!\"");
+        shell_exec("sudo /var/alarm/speech.sh \"Good Morning, Jahn.\"");
+        shell_exec("sudo /var/alarm/speech.sh \"Playing some music!\"");
         //Security risk
         $play = 'sudo mpg123 /var/www/' . $alarm["music"];
         shell_exec($play);
